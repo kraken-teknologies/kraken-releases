7c5bbf69dcac366664ac3f3a2679da7b600bbacd
Landon,Kristopher <3999130+klandon@users.noreply.github.com>
2025-12-28 18:44:24 -0500

    adding brew

--- Formula/kli.rb
+++ Formula/kli.rb
@@ -1,16 +1,31 @@
 class Kli < Formula
   desc "Kraken Command Line Interface"
-  homepage "https://github.com/krakenteknologies/kli"
-  url "https://github.com/krakenteknologies/kli/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+  homepage "https://github.com/kraken-teknologies/kli"
+  version "0.0.3"
   license "MIT"
 
-  head "https://github.com/krakenteknologies/kli.git", branch: "main"
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/kraken-teknologies/kraken-releases/releases/download/kli/v0.0.3/kli_0.0.3_darwin_arm64.tar.gz"
+      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+    else
+      url "https://github.com/kraken-teknologies/kraken-releases/releases/download/kli/v0.0.3/kli_0.0.3_darwin_amd64.tar.gz"
+      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+    end
+  end
 
-  depends_on "go" => :build
+  on_linux do
+    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+      url "https://github.com/kraken-teknologies/kraken-releases/releases/download/kli/v0.0.3/kli_0.0.3_linux_arm64.tar.gz"
+      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+    else
+      url "https://github.com/kraken-teknologies/kraken-releases/releases/download/kli/v0.0.3/kli_0.0.3_linux_amd64.tar.gz"
+      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+    end
+  end
 
   def install
-    system "go", "build", *std_go_args(ldflags: "-s -w"), "./cmd/kli"
+    bin.install "kli"
   end
 
   test do
